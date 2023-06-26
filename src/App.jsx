3d54394f98f3155aaf367576b41ab327418ee821
Jose <jvaldebenitog@me.com>
2023-06-26 00:29:38 -0400

    fix bug home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import shirtLogo from "/logoCircle.png";
 import "./styles/App.css";
+import { Link, Route } from "react-router-dom";
 
 function App() {
-  function goLogin() {
-    window.location.href = "/login";
-  }
-
   return (
     <div className="container">
       <div>
@@ -14,10 +11,12 @@ function App() {
         </a>
       </div>
       <h1 className="homeTitle">My Link Tree App</h1>
-      <button onClick={goLogin} className="homeEnterBtn">
-        <i className="material-icons">account_box</i>
-        <span>Ingresar</span>
-      </button>
+      <Link to="/login">
+        <button className="homeEnterBtn">
+          <i className="material-icons">account_box</i>
+          <span>Ingresar</span>
+        </button>
+      </Link>
       <section className="homeTextWrapper">
         <p className="homeText">
           Ingresa y manten tus enlaces de redes sociales en un solo lugar.
